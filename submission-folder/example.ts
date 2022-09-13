56044cba7aa54b9c42a8f1b5512eb2c217101304
Marcus Ong <marcus.ong.qy@gmail.com>
2022-09-13 09:40:03 +0800

    init commit

---
+++ submission-folder/example.ts
@@ -0,0 +1,6 @@
+console.log('name your submission as <your name>.ts and place it in this folder!')
+console.log('once you pushed your code, access your code from the github repo,')
+console.log(
+  'copy the link and create a card under Mission1HomePage of the AY22-23-rhdevs-frontend-training,'
+)
+console.log('and enter your name and the github link to your pushed code')
