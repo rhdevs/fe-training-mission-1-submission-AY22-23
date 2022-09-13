faad19bb3df55cb6000a1a0b88ce5441f2faf6f9
Marcus Ong <marcus.ong.qy@gmail.com>
2022-09-13 10:17:19 +0800

    update exampkle message

--- submission-folder/example.ts
+++ submission-folder/example.ts
@@ -1,6 +1,8 @@
-console.log('name your submission as <your name>.ts and place it in this folder!')
-console.log('once you pushed your code, access your code from the github repo,')
-console.log(
-  'copy the link and create a card under Mission1HomePage of the AY22-23-rhdevs-frontend-training,'
-)
-console.log('and enter your name and the github link to your pushed code')
+/**
+ * 1. Clone this repo
+ * 2. Name your submission as <your name>.ts and place it in this submission-folder folder!
+ * 3. Create your own branch, and create a PR to main
+ * 4. Wait for your PR to be approved before you can merge
+ * 5. Once merged, copy the link of your assignment code from this repo and create a card component on the main training repo under Mission1HomePage
+ * 6. Similarly, branch and PR, and merge when ready
+ */
